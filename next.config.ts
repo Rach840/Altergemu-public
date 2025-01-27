4e63414c2e1bff71edc0c979c8ecac83582e9a68
Rach840 <75145936+Rach840@users.noreply.github.com>
2025-01-27 03:13:07 +0000

    Pending changes exported from your codespace

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+  },
+  /* config options here */
 };
-
+module.exports = nextConfig;
 export default nextConfig;
